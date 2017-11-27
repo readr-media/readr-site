1b14fe1bc803ebdea75cf5a79139628f9eda2969
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-11-27 16:12:03 +0800

    implement api & testing

--- api/index.js
+++ api/index.js
@@ -1,8 +1,128 @@
-const google = require('googleapis');
+const { API_DEADLINE, API_HOST, API_PORT, API_PROTOCOL, API_TIMEOUT } = require('./config')
+const { REDIS_AUTH, REDIS_MAX_CLIENT, REDIS_READ_HOST, REDIS_READ_PORT, REDIS_WRITE_HOST, REDIS_WRITE_PORT, REDIS_TIMEOUT } = require('./config')
+const { SERVER_PROTOCOL, SERVER_HOST } = require('./config')
+const RedisConnectionPool = require('redis-connection-pool')
+const express = require('express')
+const isProd = process.env.NODE_ENV === 'production'
+const router = express.Router()
 const superagent = require('superagent')
-const urlshortener = google.urlshortener('v1');
 
-router.use('/googleLogin', function(req, res, next) {
-  console.log('got googleReq')
+const apiHost = API_PROTOCOL + '://' + API_HOST + ':' + API_PORT
+
+const fetchStaticJson = (req, res, next, jsonFileName) => {
+  const url = `${SERVER_PROTOCOL}://${SERVER_HOST}/json/${jsonFileName}.json`
+  superagent
+  .get(url)
+  .end((err, response) => {
+    if (!err && response) {
+      res.json(JSON.parse(response.text))
+    } else {
+      res.send('{\'error\':' + err + '}')
+      console.error(`error during fetch data from ${jsonFileName} : ${url}`)
+      console.error(err)  
+    }
+  })
+}
+
+const redisPoolRead = RedisConnectionPool('myRedisPoolRead', {
+  host: REDIS_READ_HOST,
+  port: REDIS_READ_PORT,
+  max_clients: REDIS_MAX_CLIENT ? REDIS_MAX_CLIENT : 50,
+  perform_checks: false,
+  database: 0,
+  options: {
+    auth_pass: REDIS_AUTH
+  }
+})
+const redisPoolWrite = isProd ? RedisConnectionPool('myRedisPoolWrite', {
+  host: REDIS_WRITE_HOST,
+  port: REDIS_WRITE_PORT,
+  max_clients: REDIS_MAX_CLIENT ? REDIS_MAX_CLIENT : 50,
+  perform_checks: false,
+  database: 0,
+  options: {
+    auth_pass: REDIS_AUTH
+  }
+}) : redisPoolRead
+const redisFetching = (url, callback) => {
+  redisPoolRead.get(decodeURIComponent(url), function (err, data) {
+    redisPoolRead.ttl(decodeURIComponent(url), (_err, _data) => {
+      if (!_err && _data) {
+        if (_data === -1) {
+          redisPoolWrite.del(decodeURIComponent(url), (_e, _d) => {
+            if (_e) {
+              console.log('deleting key ', decodeURIComponent(url), 'from redis in fail ', _e)
+            }
+          })
+        }
+      } else {
+        console.log('fetching ttl in fail ', _err)
+      }
+    })
+    callback && callback({ err, data })
+  })
+}
+const redisWriting = (url, data, callback) => {
+  redisPoolWrite.set(decodeURIComponent(url), data, function (err) {
+    if(err) {
+      console.log('redis writing in fail. ', decodeURIComponent(url), err)
+    } else {
+      redisPoolWrite.expire(decodeURIComponent(url), REDIS_TIMEOUT, function(error, d) {
+        if(error) {
+          console.log('failed to set redis expire time. ', decodeURIComponent(url), err)
+        }
+      })
+    }
+  })
+}
+
+router.all('/', function(req, res, next) {
   next()
 })
+
+router.use('/grouped', function(req, res, next) {
+  fetchStaticJson(req, res, next, 'grouped')
+})
+
+router.get('*', function(req, res) {
+  console.log(apiHost)  
+  console.log(decodeURIComponent(req.url))
+  const url = `${apiHost}${req.url}`
+  try {
+    redisFetching(req.url, ({ err, data }) => {
+      if (!err && data) {
+        res.json(JSON.parse(data))
+      } else {
+        superagent
+        .get(url)
+        .timeout(
+          {
+            response: API_TIMEOUT,  // Wait 5 seconds for the server to start sending,
+            deadline: API_DEADLINE ? API_DEADLINE : 60000, // but allow 1 minute for the file to finish loading.
+          }
+        )
+        .end((err, response) => {
+          if (!err && response) {
+            const resData = JSON.parse(response.text)
+            if (Object.keys(resData).length !== 0 && resData.constructor === Object) {
+              redisWriting(req.url, response.text)
+            }
+            res.json(resData)
+          } else {
+            res.json(err)
+            console.error(`error during fetch data from : ${url}`)
+            console.error(err)  
+          }
+        })
+      }
+    })
+  } catch (e) {
+    res.send(e)
+    console.error(`error during fetch data from api : ${req.url}`)
+    console.error(e)
+  }
+})
+
+
+
+module.exports = router
