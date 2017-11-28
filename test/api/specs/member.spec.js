4338ad6362e95249e73c1893df39ade3bd32bea4
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-11-28 18:44:35 +0800

    temp

--- test/api/specs/member.spec.js
+++ test/api/specs/member.spec.js
@@ -4,10 +4,10 @@ const api = supertest('http://localhost:8080')
 const chai = require('chai')
 const should = chai.should()
 
-describe('test member data fetching', () => {
+describe('GET/POST/PUT/DELETE to /member', () => {
   it('should get member data from api', (done) => {
     api.get('/api/member/abc1231')
-    .end(function(err, res) {
+    .end(function (err, res) {
       if (err) return console.log(err)
       res.body.should.be.an('object').and.have.property('id')
       res.body.should.have.property('name')
@@ -31,6 +31,38 @@ describe('test member data fetching', () => {
       res.body.should.have.property('comment_push')
       res.body.should.have.property('active')
       done()
+    })
+  })
+  it('should post sucessfully', () => {
+    const faker = require('faker')
+    const id = `test${Math.round(Math.random() * 10000)}`
+    api.post('/member')
+    .send({
+      'id': id,
+      'name': faker.name.findName(),
+      'nickname': faker.name.title(),
+      'birthday': faker.date.past(),
+      'gender': 'M',
+      'occupation': faker.name.jobTitle(),
+      'mail': faker.internet.email(),
+      'register_mode': null,
+      'social_id': null,
+      'created_at': '2017-09-29T20:23:45Z',
+      'updated_at': '2017-11-08T04:28:34Z',
+      'updated_by': null,
+      'description': null,
+      'profile_image': null,
+      'identity': null,
+      'custom_editor': false,
+      'hide_profile': false,
+      'profile_push': false,
+      'post_push': false,
+      'comment_push': false,
+      'active': false      
+    })
+    .end(function (err, res) {
+      if (err) return console.log(err)
+      console.log(res)
     })
   })
 })
