a008de1f080f3cec448b46404af0d16898936a07
Noah Tan <tempo0829@gmail.com>
2017-12-20 16:32:45 +0800

    add normalize css

---
+++ src/views/Account.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="accountPage">
+    <account-panel></account-panel>
+  </div>
+</template>
+<script>
+  import AccountPanel from '../components/AccountPanel.vue'
+
+  export default {
+    name: 'AccountPage',
+    components: {
+      AccountPanel
+    },
+    mounted () {},
+    methods: {}
+  }
+</script>
+<style lang="stylus" scoped>
+</style>
