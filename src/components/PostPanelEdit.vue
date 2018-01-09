08c28a89e39a9a10b438712079dd42b70c2056c8
Noah Tan <tempo0829@gmail.com>
2018-01-09 15:46:43 +0800

    add editor feature (#54)

--- src/components/PostPanelEdit.vue
+++ src/components/PostPanelEdit.vue
@@ -2,19 +2,45 @@
   <section class="postPanelEdit">
     <input v-model="title" type="text" class="postPanelEdit__title" placeholder="輸入標題">
     <app-editor :contentEdit="content" :needReset="resetToggle" v-on:updateContent="$_postPanelEdit_updateContent"></app-editor>
-    <div class="postPanelEdit__link">
+    <div class="postPanelEdit__input postPanelEdit__link">
       <label for="">新聞連結：</label>
       <input v-model="link" type="url">
+    </div>
+    <div class="postPanelEdit__input postPanelEdit--publishDate">
+      <label for="">發佈日期：</label>
+      <no-ssr>
+        <datepicker v-model="date" :format="dateFormat" :input-class="'datepicker__input'" :language="'zh'"></datepicker>
+      </no-ssr>
+    </div>
+    <div class="postPanelEdit__input">
+      <label for="">分享標題：</label>
+      <input v-model="ogTitle" type="text">
+    </div>
+    <div class="postPanelEdit__input">
+      <label for="">分享說明：</label>
+      <input v-model="ogDescription" type="text">
+    </div>
+    <div class="postPanelEdit__input">
+      <label for="">分享縮圖：</label>
+      <input v-model="ogImage" type="text" readonly>
+      <button class="postPanelEdit__btn--img" @click="$_postPanelEdit_uploadImage"><img src="/public/icons/upload.png" alt="上傳"></button>
+      <button class="postPanelEdit__btn--img" @click="$_postPanelEdit_cleanOgImage"><img src="/public/icons/delete.png" alt="刪除"></button>
+    </div>
+    <div v-show="ogImage" class="postPanelEdit__ogImg">
+      <img :src="ogImage" :alt="ogTitle">
     </div>
     <div class="postPanelEdit__submit">
       <div class="postPanelEdit__btn draft" @click="$_postPanelEdit_submit()">存成草稿</div>
       <div class="postPanelEdit__btn submit" @click="$_postPanelEdit_submit(2)">提交</div>
+      <div class="postPanelEdit__btn" @click="$_postPanelEdit_submit(1)">發佈</div>
     </div>
   </section>
 </template>
 <script>
   import _ from 'lodash'
+  import Datepicker from 'vuejs-datepicker'
   import Editor from './Editor.vue'
+  import NoSSR from 'vue-no-ssr'
   
   const addPost = (store, params) => {
     return store.dispatch('ADD_POST', { params })
@@ -24,10 +50,16 @@
     return store.dispatch('UPDATE_POST', { params })
   }
 
+  const uploadImage = (store, file) => {
+    return store.dispatch('UPLOAD_IMAGE', { file })
+  }
+
   export default {
     name: 'PostPanelEdit',
     components: {
-      'app-editor': Editor
+      'app-editor': Editor,
+      'datepicker': Datepicker,
+      'no-ssr': NoSSR
     },
     props: {
       status: {
@@ -45,35 +77,53 @@
     data () {
       return {
         content: '',
+        date: '',
+        dateFormat: 'yyyy/MM/d',
         link: '',
+        ogDescription: '',
+        ogImage: '',
+        ogTitle: '',
         resetToggle: true,
         title: ''
       }
     },
     computed: {
-      isChange () {
-        return !_.isEmpty(_.trim(this.link)) || !_.isEmpty(_.trim(this.title)) || !_.isEmpty(_.trim(_.replace(this.content, /<[^>]*>/g, '')))
+      isEmpty () {
+        return _.isEmpty(_.trim(this.link)) && _.isEmpty(_.trim(this.title)) && _.isEmpty(_.trim(_.replace(this.content, /<[^>]*>/g, '')))
+      },
+      postActive () {
+        return _.get(this.post, [ 'active' ])
       }
     },
     watch: {
       post (val) {
-        this.title = _.get(val, [ 'title' ])
         this.content = _.get(val, [ 'content' ])
         this.link = _.get(val, [ 'link' ])
+        this.title = _.get(val, [ 'title' ])
+        this.ogDescription = _.get(val, [ 'og_description' ])
+        this.ogImage = _.get(val, [ 'og_image' ])
+        this.ogTitle = _.get(val, [ 'og_title' ])
       },
       showLightBox (val) {
         if (!val) {
-          if (this.isChange) {
-            this.$_postPanelEdit_submit()
+          if (!this.isEmpty) {
+            this.$_postPanelEdit_submit(this.postActive)
           }
         }
       }
     },
     mounted () {},
     methods: {
+      $_postPanelEdit_cleanOgImage () {
+        this.ogImage = ''
+      },
       $_postPanelEdit_resetContent () {
         this.content = ''
+        this.date = ''
         this.link = ''
+        this.ogDescription = ''
+        this.ogImage = ''
+        this.ogTitle = ''
         this.title = ''
         this.resetToggle = !this.resetToggle
       },
@@ -81,10 +131,14 @@
         const params = {}
         params.active = status
         params.author = _.get(this.$store.state, [ 'profile', 'id' ])
-        params.updated_by = _.get(this.$store.state, [ 'profile', 'id' ])
-        params.title = this.title
         params.content = this.content
         params.link = this.link
+        params.og_description = this.ogDescription
+        params.og_image = this.ogImage
+        params.og_title = this.ogTitle || this.title
+        // params.published_at = this.date
+        params.title = this.title
+        params.updated_by = _.get(this.$store.state, [ 'profile', 'id' ])
         if (this.status === 'add') {
           addPost(this.$store, params)
             .then(() => {
@@ -105,6 +159,26 @@
       },
       $_postPanelEdit_updateContent (content) {
         this.content = content
+      },
+      $_postPanelEdit_uploadImage () {
+        const input = document.createElement('input')
+        input.setAttribute('type', 'file')
+        input.setAttribute('accept', 'image/*')
+        input.click()
+        input.onchange = () => {
+          const file = input.files[0]
+          if (/^image\//.test(file.type)) {
+            const fd = new FormData()
+            fd.append('image', file)
+            uploadImage(this.$store, fd)
+              .then((res) => {
+                this.ogImage = res.body.url
+              })
+              .catch((err) => {
+                console.log(err)
+              })
+          }
+        }
       }
     }
   }
@@ -122,7 +196,7 @@
     color #000
     font-size 18px
     font-weight 600
-  &__link
+  &__input
     display flex
     width 100%
     margin-top 10px
@@ -134,6 +208,23 @@
       width 440px
       height 25px
       color #808080
+    button
+      width 25px
+      height 25px
+      img
+        width 100%
+        height auto
+  &__ogImg
+    padding-left 80px
+    margin-top 10px
+    img
+      width 180px
+      height auto
+  &--publishDate
+    margin-top 26px
+    .vdp-datepicker
+      flex 1
+      
   &__submit
     display flex
     justify-content space-between
@@ -143,7 +234,7 @@
     display inline-flex
     justify-content center
     align-items center
-    width 340px
+    width 210px
     height 30px
     margin 0
     font-size 14px
@@ -153,6 +244,16 @@
     user-select none
     &.submit
       color #fff
-      background-color #4280a2
+      background-color #808080
+    &--img
+      padding 0
+      margin 0 5px
+      background none
+      border none
+      cursor pointer
+      outline none
+      user-select none
+      &:first-of-type
+        margin-left 10px
 
 </style>
