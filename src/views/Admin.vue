6b3383c0b1eb23d10f880d5479e1afbe8a34907e
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-01-15 11:54:03 +0800

    migrate posts panel to admin page (#73)

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,9 +3,16 @@
     <app-header :sections="sections"></app-header>
     <About :profile="profile"></About>
     <div class="control-bar">
-      <TheBaseControlBar @manageAccount="manageMember" @addAccount="addMember"></TheBaseControlBar>
+      <TheBaseControlBar @openPanel="openPanel" @addAccount="addMember"></TheBaseControlBar>
     </div>
-    <MembersPanel v-if="$can('memberManage')" @filterChanged="filterChanged"></MembersPanel>
+    <template v-if="activePanel === 'accounts'">
+      <MembersPanel v-if="$can('memberManage')" @filterChanged="filterChanged"></MembersPanel>
+    </template>
+    <template v-else-if="activePanel === 'posts'">
+      <section class="panel">
+        <post-list :posts="posts"></post-list>
+      </section>
+    </template>
     <BaseLightBox :showLightBox.sync="showLightBox" borderStyle="nonBorder" :isConversation="true">
       <MemberAccountEditor @updated="filterChanged" :shouldShow="showLightBox" :title="wording.WORDING_ADMIN_MEMBER_EDITOR_ADD_MEMBER" action="add"></MemberAccountEditor>
     </BaseLightBox>
@@ -20,11 +27,22 @@
   import AppHeader from '../components/AppHeader.vue'
   import MembersPanel from '../components/admin/MembersPanel.vue'
   import MemberAccountEditor from '../components/admin/MemberAccountEditor.vue'
+  import PostList from '../components/PostList.vue'
   import TheBaseControlBar from '../components/TheBaseControlBar.vue'
 
   const MAXRESULT = 20
   const DEFAULT_PAGE = 1
   const DEFAULT_SORT = '-updated_at'
+
+  const fetchPosts = (store, id) => {
+    return store.dispatch('GET_POSTS', {
+      params: {
+        where: {
+          author: id
+        }
+      }
+    })
+  }
 
   const getMembers = (store, { page, sort }) => {
     return store.dispatch('GET_MEMBERS', {
@@ -39,6 +57,7 @@
   export default {
     components: {
       'app-header': AppHeader,
+      'post-list': PostList,
       About,
       BaseLightBox,
       MembersPanel,
@@ -46,6 +65,9 @@
       TheBaseControlBar
     },
     computed: {
+      posts () {
+        return _.get(this.$store, [ 'state', 'posts', 'items' ], [])
+      },
       profile () {
         return _.get(this.$store, [ 'state', 'profile' ], {})
       },
@@ -55,6 +77,7 @@
     },
     data () {
       return {
+        activePanel: 'accounts',
         currPage: 1,
         currSort: '-updated_at',
         showLightBox: false,
@@ -76,8 +99,16 @@
           sort: this.currSort
         })
       },
-      manageMember () {
-
+      openPanel (panel) {
+        switch (panel) {
+          case 'accounts':
+            this.activePanel = 'accounts'
+            break
+          case 'posts':
+            this.activePanel = 'posts'
+            fetchPosts(this.$store, _.get(this.profile, [ 'id' ]))
+            break
+        }
       }
     },
     mounted () {},
@@ -94,8 +125,15 @@
     .control-bar
       width 100%
       margin 0 auto
+    .panel
+      width 100%
+      padding 22px 84px 33px
+      border 5px solid #d8ca21
+      margin 0 auto
   @media (min-width 950px)
     .admin
       .control-bar
+        max-width 950px
+      .panel
         max-width 950px
 </style>
