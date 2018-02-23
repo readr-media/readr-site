3c1539bb1a49ed84fd435f454e064da349499711
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-02-23 14:03:36 +0800

    adjust padding-top (#163)

---
+++ src/views/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search">
+    search
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Search',
+    methods: {},
+    mounted () {
+
+    },
+  }
+</script>
+<style lang="stylus" scoped></style>
