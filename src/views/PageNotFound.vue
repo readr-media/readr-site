043943f939969eb87b00ad7ccb1d647899abd03e
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-03-29 10:25:41 +0800

    implment 400/500 page (#234)

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-not-found"></div>
+</template>
+<script>
+  export default {
+    name: 'PageNotFound',
+    methods: {},
+    mounted () {},
+  }
+</script>
+<style lang="stylus" scoped>
+  .page-not-found
+    height 100vh
+    width 100vw
+    background-image url('/public/404.jpg')
+    background-position center center
+    background-size cover
+    background-repeat no-repeat
+</style>
---
+++ src/views/ServerError.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="server-error"></div>
+</template>
+<script>
+  export default {
+    name: 'ServerError',
+    methods: {},
+    mounted () {},
+  }
+</script>
+<style lang="stylus" scoped>
+  .server-error
+    height 100vh
+    width 100vw
+    background-image url('/public/500.jpg')
+    background-position center center
+    background-size cover
+    background-repeat no-repeat
+</style>
