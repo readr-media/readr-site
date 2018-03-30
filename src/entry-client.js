eaafff1c115b7a9303bf37a83fe1bf2b9fa0ddf0
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-03-30 10:50:32 +0800

    fix permission identification mechanism bug on client-side

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import 'es6-promise/auto'
+import { ROLE_MAP, } from './constants'
 import { createApp, } from './app'
+import { filter, get, } from 'lodash'
 import ProgressBar from './components/ProgressBar.vue'
 
 // global progress bar
 const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
 document.body.appendChild(bar.$el)
 
+const debug = require('debug')('CLIENT:entry-client')
+let isInitialized = false
+
 // a global mixin that calls `asyncData` when a route component's params change
 Vue.mixin({
+  beforeRouteEnter (to, from, next) {
+    debug('router link enter somewhere.', to, from)
+    next(vm => {
+      debug('isInitialized is true', isInitialized)
+      if (!isInitialized) {
+        isInitialized = true        
+      } else {
+        const role = get(filter(ROLE_MAP, { key: get(vm, '$store.state.profile.role'), }), [ 0, 'route', ], 'visitor')
+        const permission = get(to, [ 'meta', 'permission', ])
+        debug('role/permission', role, permission)
+        if (permission && (role === 'visitor' || (permission !== role && permission !== 'member'))) {
+          debug('Forbidden to go "to".')
+          next(from)
+        }
+      }
+    })
+  },
   beforeRouteUpdate (to, from, next) {
+    debug('router link to somewhere.', to.fullPath)
     const { asyncData, } = this.$options
     if (asyncData) {
       asyncData({
@@ -31,6 +54,7 @@ if (window.__INITIAL_STATE__) {
 }
 
 if (store.state.unauthorized) {
+  debug('entry-client resolved.')
   delete store.state.unauthorized
   router.push(store.state.targ_url)
 }
