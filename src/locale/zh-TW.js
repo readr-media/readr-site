c6d544d167bdd66c12f6df36a255e9195d668144
Noah Tan <tempo0829@gmail.com>
2018-05-08 12:42:33 +0800

    update project in progress typo and img

--- src/locale/zh-TW.js
+++ src/locale/zh-TW.js
@@ -103,7 +103,7 @@ export default {
   'PROJECT': {
     'DEDUCTING': '扣除中',
     'DISCUSS': '加入討論',
-    'ENCOURAGE': '鼓勵一下',
+    'ENCOURAGE': '參與專題製作',
     'HOT_KEYWORD': '熱門關鍵字',
     'JOIN_CONFIRM': '確認加入',
     'JOIN_CONTENT_1': '快來開放編輯室參與新聞製作',
