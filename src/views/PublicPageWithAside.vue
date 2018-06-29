ae5b3eaa772312a2053d66744679f291901a105b
Noah Tan <tempo0829@gmail.com>
2018-06-29 17:50:44 +0800

    update report api param (#531)

--- src/views/PublicPageWithAside.vue
+++ src/views/PublicPageWithAside.vue
@@ -78,7 +78,7 @@ const fetchReportsList = (store, {
     params: {
       max_result: max_result,
       where: {
-        publish_status: REPORT_PUBLISH_STATUS.PUBLISHED,
+        report_publish_status: REPORT_PUBLISH_STATUS.PUBLISHED,
         project_publish_status: PROJECT_PUBLISH_STATUS.PUBLISHED,
       },
       sort: sort,
