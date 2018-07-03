f20665cf510a003996b7b6b7ce84205564defed4
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-07-03 15:06:32 +0800

    setup strip key in config (#535)

--- server.js
+++ server.js
@@ -209,7 +209,8 @@ function render (req, res, next) {
       TAG_ACTIVE: config.TAG_ACTIVE, 
       GOOGLE_RECAPTCHA_SITE_KEY: config.GOOGLE_RECAPTCHA_SITE_KEY,
       DOMAIN: config.DOMAIN,
-      HOST: `${config.SERVER_PROTOCOL}://${config.SERVER_HOST}`
+      HOST: `${config.SERVER_PROTOCOL}://${config.SERVER_HOST}`,
+      STRIPE_KEY: config.STRIPE_KEY,
     } 
   }
   const handleError = err => {
