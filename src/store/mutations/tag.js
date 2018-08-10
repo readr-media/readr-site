646ee5f80e46a077d05cd60eb67c9a20fd015e53
Noah Tan <tempo0829@gmail.com>
2018-08-10 18:29:28 +0800

    add relateds by tag feature to backstage tag list

---
+++ src/store/mutations/tag.js
@@ -0,0 +1,28 @@
+// const debug = require('debug')('CLIENT:mutations:tag')
+
+const SET_PUBLIC_TAGS = (state, { tags, }) => {
+  state['publicTags'] = tags || []
+}
+
+const SET_TAGS = (state, { tags, }) => {
+  state['tags'] = tags || []
+}
+const INIT_TAGS_MOUSEEVENT = (state, { tags, }) => {
+  let obj = {}
+  tags.forEach(tag => { obj[tag.id] = false })
+  state['tagsIsMouseover'] = obj
+}
+const SET_TAGS_MOUSEEVENT = (state, { id, value, }) => {
+  state['tagsIsMouseover'][id] = value
+}
+const SET_TAGS_COUNT = (state, { meta, }) => {
+  state['tagsCount'] = meta.total
+}
+
+export {
+  SET_PUBLIC_TAGS,
+  SET_TAGS,
+  INIT_TAGS_MOUSEEVENT,
+  SET_TAGS_MOUSEEVENT,
+  SET_TAGS_COUNT,
+}
