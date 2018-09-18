5c7df88b973be0e0afbd2f2d25e174adfabd5251
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-09-18 16:37:27 +0800

    fix fb/g+ login issue

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -23,7 +23,9 @@
       </div>
       <Notification class="header__item"></Notification>
       <div v-if="isClientSide && !isLoggedIn" class="header__item header--status">
-        <router-link to="/login" v-text="$t('HEADER.LOGIN')"></router-link>
+        <!--router-link to="/login" v-text="$t('HEADER.LOGIN')"></router-link-->
+        <!--Use Alink for loading facebook/google sdk-->
+        <a  href="/login"v-text="$t('HEADER.LOGIN')"></a>
       </div>
       <a href="https://www.mirrormedia.mg/" target="_blank">
         <img class="header__item header__item--border-less" src="/public/icons/mirrormedia.png" alt="">
