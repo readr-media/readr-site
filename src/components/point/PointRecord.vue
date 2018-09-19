bf563344960d14c5f320f3685b097ecebb98ade3
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-09-19 13:33:03 +0800

    revise the point-records layout

--- src/components/point/PointRecord.vue
+++ src/components/point/PointRecord.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="point-record">
+    <div class="item title">
+      <div class="item__timestamp"><span v-text="$t('point.RECORD_TITLE.DATETIME')"></span></div>
+      <div class="item__subject">
+        <span class="object-type" v-text="$t('point.RECORD_TITLE.ITEM')"></span>
+      </div>
+      <div class="item__deduction">
+        <span class="unit" v-text="$t('point.RECORD_TITLE.MUTATION')"></span>
+      </div>
+      <div class="item__rest">
+        <span class="unit" v-text="$t('point.RECORD_TITLE.REST')"></span>
+      </div>
+    </div>
     <template v-for="item in records">
       <PointRecordItem class="point-record-item" :record="item"></PointRecordItem>
     </template>
@@ -11,7 +23,7 @@
 
   const fetchPointRecords = (store) => store.dispatch('GET_POINT_HISTORIES', {
     params: {
-      pay_type: 'consumption',
+      // pay_type: 'consumption',
     },
   })
 
@@ -31,3 +43,45 @@
     },
   }
 </script>
+<style lang="stylus" scoped>
+  .point-record
+    & >>> .item
+      width 100%
+      display flex
+      border-top 1px solid #d3d3d3
+      padding 10px 0
+      align-items center
+      &.title
+        > div
+          font-size 1rem
+          font-weight 600
+      &:last-child
+        border-bottom 1px solid #d3d3d3 
+      > div
+        margin-right 30px
+        font-size 0.9375rem
+        font-weight normal
+        line-height normal
+        &:last-child
+          margin-right 0
+      &__deduction, &__rest
+        width 110px
+        text-align right
+        .value
+          font-size 1.5625rem
+          font-weight 600
+          color #11b8c9
+          &.negative
+            color #d0021b
+        .unit
+          margin-left 10px
+      &__timestamp
+        width 160px
+      &__subject
+        flex 1
+        .object-type
+          margin-right 5px
+          // font-weight 200
+        .object-name
+          font-weight 800
+</style>
