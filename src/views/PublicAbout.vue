db2177e7efa71b5f8d6b4c10fc20f63c6a47ab01
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-09-27 18:33:52 +0800

    put on about meta (#652)

--- src/views/PublicAbout.vue
+++ src/views/PublicAbout.vue
@@ -3,11 +3,28 @@
 </template>
 
 <script>
-  import AboutContent from '../components/about/AboutContent.vue'
+  import AboutContent from 'src/components/about/AboutContent.vue'
   import VueCookie from 'vue-cookie'
 
   export default {
     name: 'PublicAbout',
+    metaInfo () {
+      const ABOUT_READER = this.$i18n
+        ? this.$t('ABOUT.WHAT_READR.PARAGRAPH_1')
+        + this.$t('ABOUT.WHAT_READR.PARAGRAPH_2')
+        + this.$t('ABOUT.WHAT_READR.PARAGRAPH_3')
+        + this.$t('ABOUT.WHAT_READR.PARAGRAPH_4')
+        + this.$t('ABOUT.WHAT_READR.PARAGRAPH_5')
+        + this.$t('ABOUT.WHAT_READR.PARAGRAPH_6')
+        + this.$t('ABOUT.WHAT_READR.PARAGRAPH_7')
+        : ''
+      return {
+        description: ABOUT_READER || 'Readr',
+        ogTitle: this.$i18n ? this.$t('OG.ABOUT') : 'Readr',
+        title: this.$i18n ? this.$t('OG.ABOUT') : 'Readr',
+        metaUrl: this.$route.path,
+      }
+    },    
     components: {
       AboutContent,
     },
