eff76ebc17bbb18276918f85ae39278d64a27fe3
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-10-05 16:59:11 +0800

    fix meta og:image path issue (#676)

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,6 +6,7 @@ export const PROJECTS = projectList
 export const SITE_DOMAIN = 'readr.tw'
 export const SITE_DOMAIN_DEV = 'dev.readr.tw'
 export const SITE_NAME = '讀+ Readr'
+export const SITE_FULL = 'https://www.readr.tw'
 
 export const URL_FB_FANPAGE = 'https://www.facebook.com/readr.tw'
 export const URL_TWITTER = 'https://twitter.com/READr_news'
