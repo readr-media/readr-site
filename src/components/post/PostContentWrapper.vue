e31ec1c4016ebb075bb5722777b12b43e1c9c95f
Kai-Wen, Hsiung <lion15945@gmail.com>
2018-12-04 15:59:23 +0800

    refactor PostContent component

---
+++ src/components/post/PostContentWrapper.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="wrapper">
+    <slot :postInstance="postInstance"></slot>
+    <AppArticleNav
+      :showFollow="false"
+      :postId="post.id"
+      :postRefId="get(post, 'project.id')"
+      :resource="post.flag"
+      :resourceType="resourceType"
+      :slug="get(post, 'flag') === 'report'? post.slug : ''"
+      :tags="post.tags"
+      :commentCount="commentCount"
+      :commentsLatest="post.comments"
+      :isNotLightbox="true"
+      :shouldShowComment="true"
+    >
+      <TagNav
+        v-if="post.tags && post.tags.length > 0"
+        slot="tagNav"
+        :tags="post.tags"
+        class="wrapper__tag-nav"
+      />
+    </AppArticleNav>
+  </div>
+</template>
+
+<script>
+import AppArticleNav from 'src/components/AppArticleNav.vue'
+import TagNav from 'src/components/tag/TagNav.vue'
+import { get, } from 'lodash'
+import { createPost, } from 'src/util/post'
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    AppArticleNav,
+    TagNav,
+  },
+  computed: {
+    postInstance () {
+      return createPost(this.post)
+    },
+    resourceType () {
+      return get(this.postInstance, [ 'processed', 'resourceType', ], '')
+    },
+    commentCount () {
+      return get(this.postInstance, [ 'processed', 'commentCount', ], 0)
+    },
+  },
+  methods: {
+    get,
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+  &__tag-nav
+    margin-top 20px
+    padding-top 10px
+    border-top 1px solid #d3d3d3
+</style>
+
+
