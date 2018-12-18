16b06407d1ff8fde7229243184a7fc8130d27a3c
Kai-Wen, Hsiung <lion15945@gmail.com>
2018-12-18 17:43:14 +0800

    add /plugins route and add components for /plugins/latest

---
+++ src/components/plugins/PluginLatest.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="latest">
+    <div class="latest__logo-wrapper logo-wrapper">
+      <a :href="SITE_FULL" target="_blank">
+        <img class="logo-wrapper__logo" src="/public/icons/readr-logo-dark.svg" alt="">
+      </a>
+    </div>
+    <PluginLatestList
+      class="latest__list"
+      :title="$t('PLUGINS.CUSTOM_EDITORS')"
+      :listItems="customEditors"
+    />
+    <PluginLatestList
+      class="latest__list"
+      :title="$t('PLUGINS.LATEST_PROJECTS')"
+      :listItems="publicProjects"
+    />
+  </div>
+</template>
+
+<script>
+import { get, take, } from 'lodash'
+import { mapState, } from 'vuex'
+import { PROJECT_PUBLISH_STATUS, PROJECT_STATUS, } from '../../../api/config'
+import { SITE_FULL, } from 'src/constants'
+import PluginLatestList from 'src/components/plugins/PluginLatestList.vue'
+
+const DEFAULT_PAGE = 1
+const DEFAULT_SORT = 'project_order,-updated_at'
+const MAXRESULT = 2
+const fetchProjectsList = (store, {
+  max_result = MAXRESULT,
+  page = DEFAULT_PAGE,
+  sort = DEFAULT_SORT,
+} = {}) => {
+  return store.dispatch('GET_PUBLIC_PROJECTS', {
+    params: {
+      max_result: max_result,
+      page: page,
+      sort: sort,
+      where: {
+        status: [ PROJECT_STATUS.DONE, PROJECT_STATUS.WIP, ],
+        publish_status: PROJECT_PUBLISH_STATUS.PUBLISHED,
+      },
+    },
+  })
+}
+
+const getMembersPublic = (store, params) => {
+  return store.dispatch('GET_PUBLIC_MEMBERS', {
+    params: params,
+  })
+}
+
+export default {
+  components: {
+    PluginLatestList,
+  },
+  data () {
+    return {
+      SITE_FULL,
+      limitCustomEditors: 2,
+      limitPublicProjects: 2,
+    }
+  },
+  computed: {
+    ...mapState({
+      customEditors (state) {
+        return take(get(state, [ 'customEditors', 'items', ], []), this.limitCustomEditors)
+      },
+      publicProjects (state) {
+        return take(get(state, [ 'publicProjects', 'normal', ], []), this.limitPublicProjects)
+      },
+    }),
+  },
+  beforeMount () {
+    getMembersPublic(this.$store, {
+      custom_editor: true,
+    })
+    fetchProjectsList(this.$store)
+  },
+  mounted () {
+    // JavaScript file that needs placing in the page contained within your iFrame
+    require('iframe-resizer/js/iframeResizer.contentWindow.min.js')
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.latest
+  border .5px solid #4a4a4a
+  padding 8px 10px
+  &__list
+    & + &
+      margin 14px 0 0 0
+
+.logo-wrapper
+  display flex
+  justify-content center
+  align-items center
+  margin 0 0 10px 0
+  &__logo
+    width 30px
+</style>
---
+++ src/components/plugins/PluginLatestListItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <li class="list-item">
+    <a class="list-item__wrapper wrapper" :href="href" target="_blank">
+      <div class="wrapper__img-wrapper img-wrapper">
+        <img :src="img" class="img-wrapper__img">
+      </div>
+      <p class="wrapper__title" v-text="title"></p>
+    </a>
+  </li>
+</template>
+
+<script>
+import { get, } from 'lodash'
+import { getArticleAuthorNickname, getArticleAuthorThumbnailImg, } from 'src/util/comm'
+import { SITE_FULL, } from 'src/constants'
+
+export default {
+  props: {
+    item: {
+      type: Object,
+      default () {
+        return {}
+      },
+    },
+  },
+  computed: {
+    isEditorsProfile () {
+      return get(this.item, 'nickname', '') !== ''
+    },
+    href () {
+      return this.isEditorsProfile ? `${SITE_FULL}/profile/${get(this.item, 'id', '')}` : `${SITE_FULL}/series/${get(this.item, 'slug', '')}`
+    },
+    title () {
+      const title = get(this.item, 'title', '')
+      const nickname = getArticleAuthorNickname(this.item)
+      return this.isEditorsProfile ? nickname : title
+    },
+    img () {
+      return this.isEditorsProfile ? getArticleAuthorThumbnailImg(this.item) : this.item.heroImage
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+  &__title
+    font-size 10px
+    line-height 1.5
+    text-align center
+    color #000000
+    margin 5px 0
+
+.img-wrapper
+  position relative
+  padding-bottom 67.25% /* 57.5 / 85.5 */
+  height 0
+  background-color #444746
+  &__img
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    object-fit contain
+</style>
