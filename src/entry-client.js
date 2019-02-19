c4035c2ded2e46c8ee908722cb461e38c9d9e5da
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2019-02-19 14:03:44 +0800

    trimming

--- src/entry-client.js
+++ src/entry-client.js
@@ -155,7 +155,6 @@ router.onReady(() => {
 
 // service worker
 if ('https:' === location.protocol && navigator.serviceWorker) {
-// if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js')
   navigator.serviceWorker.ready.then(() => debug('Ready!', navigator.serviceWorker))
   navigator.serviceWorker.addEventListener('message', event => debug('Got Msg from dervice-worker!' + event.data))
