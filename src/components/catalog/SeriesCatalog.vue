58a0ae50e72d3ae9579d60322ea879971387f02c
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-09-16 12:34:31 +0800

    adjust posts sort in catalog to -updated_at

--- src/components/catalog/SeriesCatalog.vue
+++ src/components/catalog/SeriesCatalog.vue
@@ -58,7 +58,7 @@ export default {
             project_id: this.seriesId,
             page: this.currentPage,
             max_result: 10,
-            sort: 'post_order,-published_at'
+            sort: 'post_order,-updated_at'
           }
         }
       ).then(res => {
