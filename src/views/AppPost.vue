e1b78400b8b7ad80c791c7a829c3f766aee27252
yeefun <szyeefun@gmail.com>
2019-10-17 16:46:56 +0800

    modify og:description & change fb sdk version

--- src/views/AppPost.vue
+++ src/views/AppPost.vue
@@ -103,7 +103,7 @@ export default {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.6';
+            js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v4.0';
             fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
         `
