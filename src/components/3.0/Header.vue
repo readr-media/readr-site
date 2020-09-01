2cf25be05682c76d183449f6b6151218a973230b
HY Tan <hsuehyungtan@gmail.com>
2020-09-01 15:04:19 +0800

    feat(header): add 3.0 header and update favicon (#1004)

---
+++ src/components/3.0/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <ReadrLogoOfHeader />
+  </header>
+</template>
+
+<script>
+
+import ReadrLogoOfHeader from './ReadrLogoOfHeader.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    ReadrLogoOfHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+header
+  padding-top 9px
+  padding-bottom 9px
+  text-align center
+  background-color #fff
+  >>> img
+    width 48px
+</style>
